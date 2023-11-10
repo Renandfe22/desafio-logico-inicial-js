e0cd2ad394ce9464bc1a361bce7c901b0fad5f1b
renandfe22 <lealrenan@hotmail.com>
2023-11-10 00:37:25 -0300

    Projeto Concluido

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("testando")
+let heroi = "o heroi está atualmente "
+let xp = 1000
+
+const NivelBronze = "Nivel bronze"
+const NivelPrata = "Nivel Prata"
+
+for(x = 995; x <= xp; x++){
+
+    if(x <= 1000){
+        heroi = "o heroi está atualmente" + NivelBronze
+    }
+      if(x > 1000){
+        heroi = "o heroi está atualmente" + NivelPrata
+    }
+}
+
+console.log (heroi)
